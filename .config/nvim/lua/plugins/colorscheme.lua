51bf20c4b8df6bfd986dca3b7a0c335fdbe968e4
nakashimadays365 <shinsaku.nakashima@days365.co.jp>
2025-07-20 14:01:25 +0900

    colorscheme修正

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,33 @@
 return {
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
-  },
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+				on_highlights = function(hl, c)
+					hl.markdownCodeBlock = {
+						bg = nil,
+					}
+				end,
+			}
+		end,
+	},
 
-  -- color code highlights
-  {
-    "uga-rosa/ccc.nvim",
-    event = "BufReadPost",
-    config = function()
-      vim.opt.termguicolors = true
-      local ccc = require("ccc")
-      ccc.setup({
-        highlighter = {
-          auto_enable = true,
-          lsp = true,
-        },
-      })
-    end,
-  },
+	-- color code highlights
+	{
+		"uga-rosa/ccc.nvim",
+		event = "BufReadPost",
+		config = function()
+			vim.opt.termguicolors = true
+			local ccc = require("ccc")
+			ccc.setup({
+				highlighter = {
+					auto_enable = true,
+					lsp = true,
+				},
+			})
+		end,
+	},
 }
