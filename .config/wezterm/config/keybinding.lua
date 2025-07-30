72043b70b1d03746179a90f5d47ead9cdc2c869b
nakashimadays365 <shinsaku.nakashima@days365.co.jp>
2025-07-30 14:08:37 +0900

    キーコンフィグ修正と追加

--- .config/wezterm/config/keybinding.lua
+++ .config/wezterm/config/keybinding.lua
@@ -7,6 +7,10 @@ function M.apply_to_config(config)
 	config.keys = {
 		{ key = "-", mods = "CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 		{ key = "=", mods = "CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+		{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+		{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
 		{ key = "H", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Left", 5 }) },
 		{ key = "L", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Right", 5 }) },
 		{ key = "J", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Down", 5 }) },
@@ -23,12 +27,22 @@ function M.apply_to_config(config)
 		{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
 		{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
 		{
-			key = "O",
-			mods = "SHIFT|CTRL",
+			key = "w",
+			mods = "CTRL",
 			action = act.ActivateKeyTable({
 				name = "leader_w_pane_nav",
 				timeout_milliseconds = 1500,
 			}),
+		},
+		{
+			key = "s",
+			mods = "CTRL",
+			action = act.DisableDefaultAssignment,
+		},
+		{
+			key = "Enter",
+			mods = "SHIFT",
+			action = wezterm.action.SendString("\n"),
 		},
 	}
 
@@ -53,6 +67,11 @@ function M.apply_to_config(config)
 				key = "k",
 				mods = "NONE",
 				action = act.ActivatePaneDirection("Up"),
+			},
+			{
+				key = "z",
+				mods = "NONE",
+				action = act.TogglePaneZoomState,
 			},
 			{
 				key = "Escape",
