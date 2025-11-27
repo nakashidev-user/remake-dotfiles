e91be5bcbd099231c84942ee4c59df358bc1937e
nakashimadays365 <shinsaku.nakashima@days365.co.jp>
2025-11-27 15:45:59 +0900

    weztermの設定まとめて

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,5 +4,5 @@ local config = wezterm.config_builder()
 -- 各設定ファイルを読み込み、configに適用します
 require("config.setting").apply_to_config(config)
 require("config.tab").apply_to_config(config)
-require("config.keybinding").apply_to_config(config)
+-- require("config.keybinding").apply_to_config(config) NOTE:使う予定なのでコメントで残す
 return config
